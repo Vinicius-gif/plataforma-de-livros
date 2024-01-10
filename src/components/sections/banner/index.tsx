2cf1101a2ecaeed96708b3d815c111396e8d94d0
Vinicius-gif <vfpv@hotmail.com>
2024-01-10 15:01:08 -0300

    fix styles

--- src/components/sections/banner/index.tsx
+++ src/components/sections/banner/index.tsx
@@ -3,18 +3,27 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <section className="bg-[url('/library-img.jpg')]">
-      <div className="flex flex-col text-center text-white min-h-screen bg-gradient-to-r from-amber-700 to-amber-800 opacity-20">
-        <h1>Encontre o livro de sua preferência</h1>
-        <p>
+    <section
+      className="relative h-[500px] w-full bg-cover bg-center"
+      style={{ backgroundImage: 'url("/library-img.jpg")' }}
+    >
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-gradient-to-r from-amber-700 to-amber-800 opacity-20">
+        <h1 className="text-4xl font-bold mb-4">
+          Encontre o livro de sua preferência
+        </h1>
+        <p className="text-sm text-center max-w-md mb-6">
           Explore um mundo de histórias e conhecimento no nosso site de livros.
           Navegue por uma vasta seleção que abrange gêneros diversos, desde
           emocionantes romances até cativantes obras de não-ficção. Embrace a
           paixão pela leitura e encontre o livro perfeito para cada momento. Seu
           próximo capítulo começa aqui.
         </p>
-        <div>
-          <input type="text" placeholder="As Aventuras de..." />
+        <div className="max-w-md">
+          <input
+            className="w-full px-4 py-2 border border-white rounded-md"
+            type="text"
+            placeholder="As Aventuras de..."
+          />
         </div>
       </div>
     </section>
