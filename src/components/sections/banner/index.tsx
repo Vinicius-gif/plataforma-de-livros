6db80ea639d90aa401d3f2a5ef1b8fd2ed748a5f
Vinicius <vfpv@hotmail.com>
2024-01-09 21:06:47 -0300

    update styles

--- src/components/sections/banner/index.tsx
+++ src/components/sections/banner/index.tsx
@@ -3,18 +3,19 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <section>
-      <img src="/books.jpeg" alt="books" className="w-full h-80" />
-      <h1>Encontre o livro de sua preferência</h1>
-      <p>
-        Explore um mundo de histórias e conhecimento no nosso site de livros.
-        Navegue por uma vasta seleção que abrange gêneros diversos, desde
-        emocionantes romances até cativantes obras de não-ficção. Embrace a
-        paixão pela leitura e encontre o livro perfeito para cada momento. Seu
-        próximo capítulo começa aqui.
-      </p>
-      <div>
-        <input type="text" placeholder="As Aventuras de..." />
+    <section className="bg-[url('/library-img.jpg')]">
+      <div className="flex flex-col text-center text-white min-h-screen bg-gradient-to-r from-amber-700 to-amber-800 opacity-20">
+        <h1>Encontre o livro de sua preferência</h1>
+        <p>
+          Explore um mundo de histórias e conhecimento no nosso site de livros.
+          Navegue por uma vasta seleção que abrange gêneros diversos, desde
+          emocionantes romances até cativantes obras de não-ficção. Embrace a
+          paixão pela leitura e encontre o livro perfeito para cada momento. Seu
+          próximo capítulo começa aqui.
+        </p>
+        <div>
+          <input type="text" placeholder="As Aventuras de..." />
+        </div>
       </div>
     </section>
   );
