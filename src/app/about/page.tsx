21eb2057d3a71838911825869c80bda531d469ac
Vinicius-gif <vfpv@hotmail.com>
2024-01-11 14:15:39 -0300

    create AboutPage

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <div>
+        <div>
+          <h2>Sobre</h2>
+          <img src="" alt="Imagem de prateleira" />
+        </div>
+        <div>
+          <h2>Sobre LeituraHub</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            cumque eligendi omnis voluptatem totam similique sapiente rem
+            officiis perspiciatis laborum placeat accusantium, neque molestias
+            ratione ut, voluptatibus corrupti atque repellat.
+          </p>
+          <p></p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
