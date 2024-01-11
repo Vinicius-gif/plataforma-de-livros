666eff3144c73e2565365abb60719015a0152e66
Vinicius <vfpv@hotmail.com>
2024-01-10 22:34:29 -0300

    update component

--- src/components/sections/banner/index.tsx
+++ src/components/sections/banner/index.tsx
@@ -1,30 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 
+import SearchForm from '../../ui/SearchForm';
+
 const Banner = () => {
   return (
     <section
       className="relative h-[500px] w-full bg-cover bg-center"
       style={{ backgroundImage: 'url("/library-img.jpg")' }}
     >
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-gradient-to-r from-amber-700 to-amber-800 opacity-20">
+      <div
+        className="absolute inset-0 flex flex-col justify-center items-center text-white text-center"
+        style={{ background: 'rgba(251, 191, 36, 0.2)' }}
+      >
         <h1 className="text-4xl font-bold mb-4">
           Encontre o livro de sua preferência
         </h1>
-        <p className="text-sm text-center max-w-md mb-6">
+        <p className="text-sm text-center max-w-md mb-6 opacity-90">
           Explore um mundo de histórias e conhecimento no nosso site de livros.
-          Navegue por uma vasta seleção que abrange gêneros diversos, desde
-          emocionantes romances até cativantes obras de não-ficção. Embrace a
-          paixão pela leitura e encontre o livro perfeito para cada momento. Seu
-          próximo capítulo começa aqui.
+          Embrace a paixão pela leitura e encontre o livro perfeito para cada
+          momento. Seu próximo capítulo começa aqui.
         </p>
-        <div className="max-w-md">
-          <input
-            className="w-full px-4 py-2 border border-white rounded-md"
-            type="text"
-            placeholder="As Aventuras de..."
-          />
-        </div>
+        <SearchForm />
       </div>
     </section>
   );
