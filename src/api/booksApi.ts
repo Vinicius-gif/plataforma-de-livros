968e38502e2323b9c195b34911f8f741a7054e7d
Vinicius <vfpv@hotmail.com>
2024-01-14 21:58:10 -0300

    create searchBooks function

---
+++ src/api/booksApi.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import axios from 'axios';
+require('dotenv').config();
+
+const apiKey = 'AIzaSyBAX8BcfKriCQ7sTkAwXAGCn5VfpsEsDXM';
+
+export const searchBooks = async (searchTerm) => {
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const books = response.data.items;
+    return books;
+  } catch (error) {
+    console.error('Erro ao buscar livros:', error.message);
+    throw error;
+  }
+};
