7516446d9ef0e3bb28b8985616c62eff43e16678
Vinicius <vfpv@hotmail.com>
2024-01-16 20:51:34 -0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import Banner from '../components/sections/banner';
-import BookCard from '../components/ui/BookCard';
+import ListBooks from '../components/sections/listBooks';
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <BookCard
-        image="1"
-        title="Livro"
-        author="autor"
-        totalEditions="12"
-        firstEditionYear="1980"
-      />
+      <ListBooks />
     </main>
   );
 }
