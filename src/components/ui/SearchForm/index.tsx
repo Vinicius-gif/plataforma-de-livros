3583f68778288935d0939dc9a7d7c83592b40bd5
Vinicius <vfpv@hotmail.com>
2024-01-16 21:03:11 -0300

    update SearchForm

--- src/components/ui/SearchForm/index.tsx
+++ src/components/ui/SearchForm/index.tsx
@@ -3,7 +3,15 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 
+import { useSearchBookContext } from '../../../contexts/SearchBookContext';
+
 const SearchForm = () => {
+  const { searchInputValue, setSearchInputValue } = useSearchBookContext();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInputValue(e.target.value);
+  };
+
   return (
     <div className="lg:w-1/3 mx-auto p-4">
       <div className="bg-white rounded-lg overflow-hidden">
@@ -12,10 +20,12 @@ const SearchForm = () => {
             type="text"
             className="w-full p-3 borde text-gray-800 rounded-l-md"
             placeholder="As Aventuras de..."
+            value={searchInputValue}
           />
           <button
             type="submit"
             className="bg-amber-600 text-white p-3 rounded-r-md hover:bg-amber-700"
+            onClick={() => handleChange}
           >
             <FaSearch size={24} />
           </button>
