2a5092f673efd70d426087a8af30674753eff6e3
Vinicius <vfpv@hotmail.com>
2024-01-16 22:05:38 -0300

    Update ListBooks

--- src/components/sections/listBooks/index.tsx
+++ src/components/sections/listBooks/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import { Book } from '../../../@types/book';
+import { searchBooks } from '../../../api/booksApi';
+import { useSearchBookContext } from '../../../contexts/SearchBookContext';
+import BookCard from '../../ui/BookCard';
 
 const ListBooks = () => {
-  return <div>ListBooks</div>;
+  const [dataBooks, setDataBooks] = useState<Book[]>([]);
+
+  const { searchInputValue } = useSearchBookContext();
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const books = await searchBooks(searchInputValue);
+      setDataBooks(books);
+    };
+
+    fetchBooks();
+  }, [searchInputValue]);
+
+  return (
+    <section>
+      {dataBooks.map((book) => (
+        <BookCard
+          key={book.id}
+          title={book.volumeInfo.title}
+          author={book.volumeInfo.authors.join(', ')}
+          firstEditionYear={book.volumeInfo.publisher}
+          image={book.volumeInfo.imageLinks.thumbnail}
+          totalEditions={book.volumeInfo.pageCount}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default ListBooks;
