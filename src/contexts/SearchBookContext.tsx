7cfb87ee547501457c2aff57ecad49e3280a3c79
Vinicius <vfpv@hotmail.com>
2024-01-18 21:11:52 -0300

    update context

--- src/contexts/SearchBookContext.tsx
+++ src/contexts/SearchBookContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 
 // Definindo o tipo do contexto
 interface SearchBookContextProps {
@@ -8,16 +8,17 @@ interface SearchBookContextProps {
   setSearchInputValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const SearchBookContext = createContext<SearchBookContextProps | undefined>(
-  undefined
-);
+export const SearchBookContext = createContext<
+  SearchBookContextProps | undefined
+>(undefined);
 
 interface InputProviderProps {
   children: ReactNode;
 }
 
 export const SearchBookProvider = ({ children }: InputProviderProps) => {
-  const [searchInputValue, setSearchInputValue] = useState<string>('');
+  const [searchInputValue, setSearchInputValue] =
+    useState<string>('harry potter');
 
   return (
     <SearchBookContext.Provider
@@ -26,17 +27,4 @@ export const SearchBookProvider = ({ children }: InputProviderProps) => {
       {children}
     </SearchBookContext.Provider>
   );
-};
-
-// Criando um hook personalizado para utilizar o contexto
-export const useSearchBookContext = () => {
-  const context = useContext(SearchBookContext);
-
-  if (!context) {
-    throw new Error(
-      'useSearchBookContext deve ser utilizado dentro de um SearchBookProvider'
-    );
-  }
-
-  return context;
 };
