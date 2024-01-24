71090e4fe711ef0aa2406920634546d8c5aab8b8
Vinicius-gif <vfpv@hotmail.com>
2024-01-24 14:54:19 -0300

    update component

--- src/components/ui/BookCard/index.tsx
+++ src/components/ui/BookCard/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import React from 'react';
-
 interface Props {
   image: string;
   title: string;
@@ -17,19 +17,34 @@ const BookCard = ({
   firstEditionYear
 }: Props) => {
   return (
-    <div className="max-w-56 mx-auto rounded overflow-hidden shadow-lg m-4">
-      <img className="w-full h-36 object-cover" src={image} alt={title} />
-      <div className="px-4 py-2 text-center">
-        <div className="font-bold text-sm mb-1">{title}</div>
-        <p className="text-gray-700 text-xs mb-1">Author: {author}</p>
-        <p className="text-gray-700 text-xs mb-1">
-          Total Editions: {totalEditions}
-        </p>
-        <p className="text-gray-700 text-xs">
-          First Edition Year: {firstEditionYear}
-        </p>
+    <Link href="/" className="card-link">
+      <div className="max-w-64 mx-auto rounded overflow-hidden shadow-lg my-4 card">
+        {image ? (
+          <img
+            className="w-full h-36 object-cover transition-transform transform scale-100 hover:scale-110"
+            src={image}
+            alt={title}
+          />
+        ) : (
+          <img
+            className="w-full h-36 object-cover transition-transform transform scale-100 hover:scale-110"
+            src="/cover_not_found.jpg"
+            alt={title}
+          />
+        )}
+
+        <div className="px-4 py-2 text-center">
+          <div className="font-bold text-sm mb-1">{title}</div>
+          <p className="text-gray-700 text-xs mb-1">Author: {author}</p>
+          <p className="text-gray-700 text-xs mb-1">
+            Total Editions: {totalEditions}
+          </p>
+          <p className="text-gray-700 text-xs">
+            First Edition Year: {firstEditionYear}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
