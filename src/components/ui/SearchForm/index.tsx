0566ec554823a564b3302750454c09095bcfbe73
Vinicius <vfpv@hotmail.com>
2024-01-23 21:04:10 -0300

    add handleKeyPress function and input validation

--- src/components/ui/SearchForm/index.tsx
+++ src/components/ui/SearchForm/index.tsx
@@ -9,28 +9,57 @@ import { BookContext } from '../../../contexts/BookContext';
 const SearchForm = () => {
   const [inputSearch, setInputSearch] = useState('');
 
-  const { setDataBooks } = useContext(BookContext);
+  const { setDataBooks, setIsLoading } = useContext(BookContext);
 
-  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputSearch(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    setIsLoading(true);
 
     const booksData = await searchBooks(inputSearch);
+
+    setIsLoading(false);
 
     setDataBooks(booksData);
 
     setInputSearch('');
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (inputSearch.trim() !== '') {
+      handleSearch();
+    } else {
+      event.currentTarget.querySelector('input').blur();
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+
+      if (inputSearch.trim() === '') {
+        e.currentTarget.blur();
+      } else {
+        handleSearch();
+      }
+    }
+  };
+
   return (
     <div className="lg:w-1/3 mx-auto p-4">
       <div className="bg-white rounded-lg overflow-hidden">
-        <form onSubmit={handleSearch} className="flex items-center">
+        <form onSubmit={handleSubmit} className="flex items-center">
           <input
             type="text"
             className="w-full p-3 border text-gray-800 rounded-l-md"
             placeholder="As Aventuras de..."
             value={inputSearch}
-            onChange={(e) => setInputSearch(e.target.value)}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
           />
           <button
             className="bg-amber-600 text-white p-3 rounded-r-md hover:bg-amber-700"
