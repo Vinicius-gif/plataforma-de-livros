89dc3e0fe64f0f7314fe987a1fe62f5d20b2a438
Vinicius <vfpv@hotmail.com>
2024-01-28 21:30:17 -0300

    add Dialog component

--- src/components/ui/BookCard/index.tsx
+++ src/components/ui/BookCard/index.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
 import React from 'react';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '../dialog';
 interface Props {
   image: string;
   title: string;
   author: string;
   totalEditions: number;
   firstEditionYear: string;
+  description: string;
 }
 
 const BookCard = ({
@@ -14,22 +22,19 @@ const BookCard = ({
   title,
   author,
   totalEditions,
-  firstEditionYear
+  firstEditionYear,
+  description
 }: Props) => {
   return (
-    <div className="grid grid-cols-1 content-center justify-items-center h-[450px] max-w-72 w-80 mx-auto my-4 rounded overflow-hidden shadow-md hover:shadow-lg transition duration-300">
-      <Link href="/">
-        {image ? (
-          <img className="w-32 h-40" src={image} alt={title} />
-        ) : (
-          <img className="w-32 h-40" src="/cover_not_found.jpg" alt={title} />
-        )}
-      </Link>
+    <div className="grid grid-cols-1 content-center justify-items-center h-[450px] max-w-72 w-80 mx-auto my-4 rounded overflow-hidden shadow-lg hover:shadow-xl transition duration-300">
+      {image ? (
+        <img className="w-32 h-40" src={image} alt={title} />
+      ) : (
+        <img className="w-32 h-40" src="/cover_not_found.jpg" alt={title} />
+      )}
 
       <div className="px-4 py-3 text-center">
-        <Link href="/" className="font-bold text-lg mb-2 hover:underline">
-          {title}
-        </Link>
+        <h2 className="font-bold text-lg mb-2">{title}</h2>
         <p className="text-gray-700 text-sm mb-2">Author: {author}</p>
         <p className="text-gray-700 text-sm mb-2">
           Total Editions: {totalEditions}
@@ -38,9 +43,26 @@ const BookCard = ({
           First Edition Year: {firstEditionYear}
         </p>
 
-        <button className="bg-blue-500 text-white py-2 px-3 mt-3 hover:bg-blue-600">
-          Detalhes
-        </button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="bg-blue-500 text-white py-2 px-3 mt-3 hover:bg-blue-600">
+              Detalhes
+            </button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-center font-semibold text-3xl underline">
+                Detalhes do livro
+              </DialogTitle>
+              <img className="w-32 h-40" src={image} alt={title} />
+              <h2 className="font-bold text-2xl mb-2 text-center underline">
+                {title}
+              </h2>
+              <p className="font-medium">{description}</p>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
