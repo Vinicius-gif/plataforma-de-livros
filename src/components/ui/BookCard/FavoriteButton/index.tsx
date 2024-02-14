6f19d17efe09a7b71346eeb06a68a9fea999744d
Vinicius <vfpv@hotmail.com>
2024-02-14 20:31:55 -0300

    add FavoriteButton component

---
+++ src/components/ui/BookCard/FavoriteButton/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import { MdFavoriteBorder } from 'react-icons/md';
+
+import { BookProps } from '../../../../@types/BookContextTypes';
+import { useAuth } from '../../../../lib/firebase/authService';
+import { addFavoriteBook } from '../../../../lib/firebase/fireStoreService';
+
+const FavoriteButton = ({ ...bookData }: BookProps) => {
+  const { user } = useAuth();
+
+  return (
+    <div
+      className="absolute top-0 right-0 mt-4 mr-4 cursor-pointer"
+      onClick={() => addFavoriteBook(user.uid, bookData)}
+    >
+      <MdFavoriteBorder className="size-8" />
+    </div>
+  );
+};
+
+export default FavoriteButton;
