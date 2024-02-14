b1981aeee17b4dd6450461f67c54e36e0c61edb6
Vinicius <vfpv@hotmail.com>
2024-02-14 20:29:16 -0300

    update links

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -10,6 +10,7 @@ import { useAuth } from '../../../lib/firebase/authService';
 
 const Header = () => {
   const { user, logout } = useAuth();
+
   return (
     <header>
       <nav className="w-full flex justify-around items-center">
@@ -24,12 +25,24 @@ const Header = () => {
         <div className="flex items-center gap-5 [&>a]:text-lg [&>a]:font-semibold">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
+          <Link
+            href={user && user.uid ? `/favoriteBooks/${user.uid}` : '/login'}
+          >
+            Meus Favoritos
+          </Link>
 
-          <div>
+          <div className="mx-3">
             {user ? (
               <div>
-                <p>Email: {user.email}</p>
-                <Button onClick={logout}>Logout</Button>
+                <p className="font-semibold text-lg">User: {user.email}</p>
+                <Button
+                  variant="contained"
+                  size="small"
+                  className="border-2 text-base bg-red-400 hover:bg-red-500 m-2"
+                  onClick={logout}
+                >
+                  Sair
+                </Button>
               </div>
             ) : (
               <div>
