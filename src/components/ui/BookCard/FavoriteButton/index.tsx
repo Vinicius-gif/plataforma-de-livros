6e09c62fdfa9751382497b85be7438f5fe254c39
Vinicius <vfpv@hotmail.com>
2024-02-15 21:07:03 -0300

    add: functions to remove and check favorite books

--- src/components/ui/BookCard/FavoriteButton/index.tsx
+++ src/components/ui/BookCard/FavoriteButton/index.tsx
@@ -1,21 +1,49 @@
 'use client';
 
-import React from 'react';
-import { MdFavoriteBorder } from 'react-icons/md';
+import React, { useState, useEffect } from 'react';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 
 import { BookProps } from '../../../../@types/BookContextTypes';
 import { useAuth } from '../../../../lib/firebase/authService';
-import { addFavoriteBook } from '../../../../lib/firebase/fireStoreService';
+import {
+  addFavoriteBook,
+  removeFavoriteBook,
+  isBookFavorited
+} from '../../../../lib/firebase/fireStoreService';
 
 const FavoriteButton = ({ ...bookData }: BookProps) => {
+  const [favorited, setFavorited] = useState(false);
   const { user } = useAuth();
+
+  useEffect(() => {
+    const checkIfFavorited = async () => {
+      if (user) {
+        const favorited = await isBookFavorited(user.uid, bookData.id);
+        setFavorited(favorited);
+      }
+    };
+    checkIfFavorited();
+  }, [user, bookData]);
+
+  const handleButton = () => {
+    if (user) {
+      favorited
+        ? removeFavoriteBook(user.uid, bookData.id)
+        : addFavoriteBook(user.uid, bookData);
+      setFavorited(!favorited);
+    }
+  };
 
   return (
     <div
       className="absolute top-0 right-0 mt-4 mr-4 cursor-pointer"
-      onClick={() => user && addFavoriteBook(user.uid, bookData)}
+      onClick={handleButton}
     >
-      <MdFavoriteBorder className="size-8" />
+      {favorited ? (
+        <MdFavorite className="size-8" />
+      ) : (
+        <MdFavoriteBorder className="size-8" />
+      )}
     </div>
   );
 };
--- src/lib/firebase/fireStoreService.ts
+++ src/lib/firebase/fireStoreService.ts
@@ -1,18 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { collection, doc, addDoc, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  addDoc,
+  getDocs,
+  query,
+  where,
+  deleteDoc
+} from 'firebase/firestore';
 
 import { BookProps } from '../../@types/BookContextTypes';
 import { db } from './firebaseService';
-
-export const addFavoriteBook = async (userId: string, bookData: BookProps) => {
-  try {
-    const userRef = doc(collection(db, 'users'), userId);
-    await addDoc(collection(userRef, 'favoriteBooks'), bookData);
-    alert('Book added to favorites successfully!');
-  } catch (error: any) {
-    console.error('Error adding book to favorites: ', error);
-  }
-};
 
 export const getFavoriteBooks = async (
   userId: string
@@ -32,5 +30,48 @@ export const getFavoriteBooks = async (
   } catch (error: any) {
     console.error('Error getting favorite books: ', error);
     return []; // Returning an empty array in case of error
+  }
+};
+
+export const addFavoriteBook = async (userId: string, bookData: BookProps) => {
+  try {
+    const userRef = doc(collection(db, 'users'), userId);
+    await addDoc(collection(userRef, 'favoriteBooks'), bookData);
+    alert('Book added to favorites successfully!');
+  } catch (error: any) {
+    console.error('Error adding book to favorites: ', error);
+  }
+};
+
+export const removeFavoriteBook = async (userId: string, bookId: string) => {
+  try {
+    const userRef = doc(collection(db, 'users'), userId);
+    const querySnapshot = await getDocs(
+      query(collection(userRef, 'favoriteBooks'), where('id', '==', bookId))
+    );
+
+    querySnapshot.forEach(async (doc) => {
+      await deleteDoc(doc.ref);
+      alert('Book removed from favorites successfully!');
+    });
+  } catch (error: any) {
+    console.error('Error removing book from favorites: ', error);
+  }
+};
+
+export const isBookFavorited = async (
+  userId: string,
+  bookId: string
+): Promise<boolean> => {
+  try {
+    const userRef = doc(collection(db, 'users'), userId);
+    const querySnapshot = await getDocs(
+      query(collection(userRef, 'favoriteBooks'), where('id', '==', bookId))
+    );
+
+    return !querySnapshot.empty;
+  } catch (error: any) {
+    console.error('Error checking if book is favorited: ', error);
+    return false;
   }
 };
